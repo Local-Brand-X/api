1aeec4b619ad6f7bd607df9a5b042357db8b7af9
Farshid Boroomand <farshidboroomand@gmail.com>
2023-09-15 20:24:39 +0330

    employee route defined

--- routes/api_v1.php
+++ routes/api_v1.php
@@ -1,1 +1,10 @@
 <?php
+
+use Illuminate\Support\Facades\Route;
+
+Route::group([
+    'prefix' => 'employee',
+    'namespace' => 'Employees\Controllers',
+], static function () {
+    Route::post('/', 'EmployeesController@store')->name('employees.store');
+});

e227cc3e4d3acf7e8e310049a1ba3a9d37ad5ca8
Farshid Boroomand <farshidboroomand@gmail.com>
2023-09-16 16:24:17 +0330

    events and listeners and observers registered

--- app/Providers/EventServiceProvider.php
+++ app/Providers/EventServiceProvider.php
@@ -5,7 +5,10 @@ namespace App\Providers;
 use Illuminate\Auth\Events\Registered;
 use Illuminate\Auth\Listeners\SendEmailVerificationNotification;
 use Illuminate\Foundation\Support\Providers\EventServiceProvider as ServiceProvider;
-use Illuminate\Support\Facades\Event;
+use Modules\V1\Files\Events\FileUploaded;
+use Modules\V1\Files\Listeners\ImportUploadedFileData;
+use Modules\V1\Files\Models\File;
+use Modules\V1\Files\Observers\FileObserver;
 
 class EventServiceProvider extends ServiceProvider
 {
@@ -18,6 +21,9 @@ class EventServiceProvider extends ServiceProvider
         Registered::class => [
             SendEmailVerificationNotification::class,
         ],
+        FileUploaded::class => [
+            ImportUploadedFileData::class,
+        ],
     ];
 
     /**
@@ -25,6 +31,7 @@ class EventServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
+        File::observe(FileObserver::class);
     }
 
     /**
